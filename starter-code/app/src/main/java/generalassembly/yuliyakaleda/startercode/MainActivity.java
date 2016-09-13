6effff20e7a411315716dd0bdc4e77b362a7a87b
James <jamesrondina@gmail.com>
2016-09-12 22:54:00 -0400

    jrondina does what it needs to do

--- starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
+++ starter-code/app/src/main/java/generalassembly/yuliyakaleda/startercode/MainActivity.java
@@ -5,9 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+  EditText editText;
+  Button button;
+  TextView textView;
+  ListView listView;
+  ArrayList<String> list;
+  ArrayAdapter<String> adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -15,13 +28,33 @@ public class MainActivity extends AppCompatActivity {
     setContentView(R.layout.activity_main);
 
     //TODO: set up all the view and event listeners.
-  }
 
-  @Override
-  public void onClick(View v) {
-    // TODO: 1. get the text from the input field
-    //       2. animate it in the center of the screen
-    //       3. add it to the list wish
-    //       4. clear the input field
+    editText = (EditText) findViewById(R.id.editText);
+    button = (Button) findViewById(R.id.button);
+    textView = (TextView) findViewById(R.id.textView);
+    listView = (ListView) findViewById(R.id.wishList);
+
+    list = new ArrayList<>();
+
+    adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+    listView.setAdapter(adapter);
+
+
+    View.OnClickListener listener = new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+
+        textView.setText(editText.getText().toString());
+        Animation anim = AnimationUtils.loadAnimation(MainActivity.this, R.anim.animation);
+        textView.startAnimation(anim);
+
+        list.add(textView.getText().toString());
+        editText.setText("");
+        adapter.notifyDataSetChanged();
+
+      }
+    };
+
+    button.setOnClickListener(listener);
   }
 }
